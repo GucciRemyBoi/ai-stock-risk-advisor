1de1d4ccec92fb144f03283dc4e3a7308d2cfce8
GucciRemyBoi <renz.wilkins@live.com>
2025-04-16 14:32:33 -0600

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="p-10 bg-indigo-600 text-white text-3xl font-bold rounded-lg shadow-md">
+    🚀 Tailwind is working!
+  </div>
+);
 }
 
 export default App;
