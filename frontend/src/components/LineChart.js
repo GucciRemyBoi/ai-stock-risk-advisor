f8231db3496bfde5854fb3223d8567fc9f9dc6af
GucciRemyBoi <renz.wilkins@live.com>
2025-04-16 21:03:31 -0600

    changed the libaries for react, as the versions of react and react-dom weren't syncing up causing issues with the website rendering.

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -2,7 +2,8 @@ import React from 'react';
 const LineChart = () => {
     return (
         <div className="bg-white rounded-lg shadow p-4">
-            <h3 className="text-lg fonr-semibold mb-2">Line Chart (Placeholder</h3>
+            <h3 className="text-lg fonr-semibold mb-2">Line Chart (Placeholder)
+            </h3>
             <p className="text-sm text-gray-500">Live + predicted data goes here.</p>
         </div>
     );
